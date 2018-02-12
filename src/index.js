935b1575e01e20f098ccc7bb383fa4729ccb23b5
Anda <ax.fiolle@gmail.com>
2018-02-12 10:50:48 +0100

    router working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+// Pages
+import Home from './Home.js';
+import More from './More.js';
+import NotFound from './NotFound.js';
+// Rooter
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+	return (
+		<Router>
+      <div>
+        <header>
+          <h1>Axel Fiolle</h1>
+          <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/More">More</Link></li>
+            <li><Link to="/NotFound">NotFound</Link></li>
+          </ul>
+          </nav>
+        </header>
+        <Switch>
+          <Route exact path='/' component={ Home } />
+          <Route exact path='/More' component={ More } />
+          <Route component={ NotFound } />
+        </Switch>
+        <footer>
+          <p>&copy; Axel Fiolle</p>
+        </footer>
+      </div>
+    </Router>
+	)
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();

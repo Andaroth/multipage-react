2ff87443d1e74f16b58f543cc35ead3916c722df
Anda <ax.fiolle@gmail.com>
2018-02-13 23:16:41 +0100

    night fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -39,29 +39,29 @@ export default class Home extends Component {
             <section id="four" className="scrollspy">
               <h3>My story</h3>
               <Collapsible popout defaultActiveKey={0}>
-                <CollapsibleItem header='HTML5/CSS3' icon='polymer'>
+                <CollapsibleItem header='First steps with HTML and CSS' icon='polymer'>
                   <p>I wrote my first HTML line when I was very young, around twelve years old. I used a software named Nvu which allowed me to do What-You-See-Is-What-You-Get early and learn HTML/CSS basics from it.</p>
                   <p>I decided to learn coding seriously later with SiteDuZero (which became OpenClassroom). I also discovered PHP but we'll talk about it later. </p>
                   <p>Serious business started when I was at <strong>High School Albert Jacquard</strong> where I learnt <strong>HTML5</strong> and <strong>CSS3</strong>. </p>
                   <p>Now I'm barely a master in HTML5 and CSS3. When I work without any framework, my code is always 100% valid with the <strong>W3C</strong>'s standards.</p>
                 </CollapsibleItem>
-                <CollapsibleItem header='PHP' icon='settings_ethernet'>
+                <CollapsibleItem header='The first PHP project' icon='settings_ethernet'>
                   <p>I discovered PHP years ago, I made a <strong>blogroll</strong> (with a backend to post new stuff with a form, very nice) by myself when I was like fourteen. <br/>I always kept working in PHP even in front-end because I liked to include components this way. </p>
                   <p>Now I'm seriously training to work with <strong>MVC</strong> architecture.</p>
                 </CollapsibleItem>
-                <CollapsibleItem header='JavaScript &amp; jQuery' icon='movie_filter'>
+                <CollapsibleItem header='Getting introduce to JavaScript' icon='movie_filter'>
                   <p>I started to make my first own JS lines when I was at High School. We discovered it mostly with <strong>jQuery</strong> but I learnt to do <strong>vanilla JavaScript</strong> too. </p>
                   <p>I must admit that I like coding with jQuery more than JS because it allows me to focus on the action itself instead of the process hidden behind. (Yes I'm still a front-end hearted developer)</p>
                 </CollapsibleItem>
-                <CollapsibleItem header='NodeJS' icon='share'>
+                <CollapsibleItem header='Discover the power of NodeJS' icon='share'>
                   <p>I really like NodeJS, I love how it works. It's like making it's own Apache, it's own SMTP, it's own everything. <br/>I started learning NodeJS at BeCode in Bruxelles. </p>
                   <p>I trained my Node skills making <strong>http servers</strong>, <strong>webchats</strong>, stuff like that.</p>
                 </CollapsibleItem>
-                <CollapsibleItem header='Symfony' icon='folder_special'>
+                <CollapsibleItem header='Falling in love with Symfony' icon='folder_special'>
                   <p>I love it. This is the best way to work with PHP. <br/>I really feel like it totally fits with the way I was working before discovering it, making my own MVC architecture. </p>
                   <p>I discovered it recently because I needed it for an internship. I keet working with it because it rules. </p>
                 </CollapsibleItem>
-                <CollapsibleItem header='React' icon='blur_circular'>
+                <CollapsibleItem header='Now learning React' icon='blur_circular'>
                   <p>This is a great JavaScript framework. This website is made in React so <a href="mailto:contact@axelfiolle.be">tell me if it's well done</a>, please ! :D </p>
                   <div className="center-align">
                     <Button href="mailto:contact@axelfiolle.be" waves="light" node="a">Send a mail</Button>
